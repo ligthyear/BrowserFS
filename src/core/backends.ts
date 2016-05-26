b7f8161ed512830c9d03f8ffde4df2d72725f1cc
Benjamin Kampmann <ben@create-build-execute.com>
2016-05-26 16:42:23 +0200

    local ghost-in-the-safe backends

---
+++ src/core/backends.ts
@@ -0,0 +1,7 @@
+import AsyncMirror from '../backend/AsyncMirror';
+import InMemory from '../backend/InMemory';
+import LocalStorage from '../backend/LocalStorage';
+import ZipFS from '../backend/ZipFS';
+import MountableFileSystem from '../backend/MountableFileSystem';
+
+export { AsyncMirror, InMemory, LocalStorage, ZipFS, MountableFileSystem };
